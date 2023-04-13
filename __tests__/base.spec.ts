5cfae7dfa6ca00d85d69068e55c0db687ecab374
Mark Struck <markstruck@comcast.net>
2023-04-13 03:14:30 -0500

    updated es version for ts and added a new test for timers

--- __tests__/base.spec.ts
+++ __tests__/base.spec.ts
@@ -1,4 +1,40 @@
 import { evaluate, BinaryOperation, BinaryOperators } from "../src";
+const _setTimeout = window.setTimeout;
+
+const continouslyAdvanceTimers = () => {
+    let isCancelled = false;
+
+    const advance = async () => {
+        while (!isCancelled) {
+            jest.runOnlyPendingTimers();
+            await new Promise( resolve => _setTimeout(resolve) );
+        }
+    };
+
+    advance();
+    return () => {
+        isCancelled = true;
+    }    
+};
+
+describe('Sample Test', () => {
+    beforeAll(() => {
+        jest.useFakeTimers();
+    });
+
+    it('timing', async () => { 
+        const shouldResolve = Promise.resolve()
+            .then(() => console.log('before-promise'))
+            .then(() => new Promise(r => setTimeout(r, 20)))
+            .then(() => console.log('after-promise'))
+
+        setTimeout(() => console.log('timer'), 100);
+        const cancelAdvance = continouslyAdvanceTimers();
+        await shouldResolve;
+        cancelAdvance();
+        console.log('end');
+    });
+});
 
 describe("Simple expression tests", () => {
     test("Check literal value", () => {
